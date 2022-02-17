a6c5b571a00bc2f32abccc4c2422330d10c75b97
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-02-17 18:13:44 +0100

    completo con vue cli

---
+++ src/components/Main.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <Chosen :dischi="DiskArray" @selezionato="filtra" />
+    <Disk v-for="(disco, index) in generiFiltrati" :key="index" :info="disco" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Disk from "./Disk.vue";
+import Chosen from "./Chosen.vue";
+
+export default {
+  name: "Main",
+  components: {
+    Disk,
+    Chosen,
+  },
+  data() {
+    return {
+      apiUrl: " http://localhost:81/php-ajax-dischi/milestoneuno/index.php",
+      DiskArray: [],
+      valoreGenere: "",
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    filtra(selected) {
+      this.valoreGenere = selected;
+    },
+    filteredGenre() {
+      this.DiskArray;
+    },
+    getData() {
+      axios
+        .get(this.apiUrl)
+        .then((risposta) => {
+          this.DiskArray = risposta.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+  computed: {
+    generiFiltrati() {
+      return this.DiskArray.filter((genere) => {
+        console.log(this.valoreGenere);
+        return genere.genre.includes(this.valoreGenere);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+</style>
